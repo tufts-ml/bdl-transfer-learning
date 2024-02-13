2646e329e820a04a8746beb26c83e62b39ff7ed5
Ethan Harvey <ethan.harvey@tufts.edu>
2024-02-13 11:38:02 -0500

    Working on anonymizing code

--- scripts/nonlearned_experiments.sh
+++ scripts/nonlearned_experiments.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH -p hugheslab
+#SBATCH -p gpu
 #SBATCH -t 48:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64g
-#SBATCH -o /cluster/tufts/hugheslab/eharve06/slurmlog/out/log_%j.out # Write stdout to file named log_JOBIDNUM.out in log dir
-#SBATCH -e /cluster/tufts/hugheslab/eharve06/slurmlog/err/log_%j.err # Write stderr to file named log_JOBIDNUM.err in log dir
-#SBATCH --array=0-1
+#SBATCH -o /log_%j.out # Write stdout to file named log_JOBIDNUM.out
+#SBATCH -e /log_%j.err # Write stderr to file named log_JOBIDNUM.err
+#SBATCH --array=0-n # TODO: Change n to the number of experiments.
 
 source ~/.bashrc
 conda activate bdl-transfer-learning
 
 # Define an array of commands
 experiments=(
-    "python ../src/CIFAR10_main.py --dataset_path='/cluster/tufts/hugheslab/eharve06/CIFAR-10' --experiments_path='/cluster/tufts/hugheslab/eharve06/bdl-transfer-learning/experiments/retrained_CIFAR-10_torchvision' --lr_0=0.01 --model_name='nonlearned_lr_0=0.01_n=10_random_state=1001_weight_decay=0.01' --n=10 --prior_path='/cluster/tufts/hugheslab/eharve06/resnet50_torchvision' --prior_type='nonlearned' --random_state=1001 --wandb --wandb_project='retrained_CIFAR-10_torchvision' --weight_decay=0.01"
-    "python ../src/CIFAR10_main.py --dataset_path='/cluster/tufts/hugheslab/eharve06/CIFAR-10' --experiments_path='/cluster/tufts/hugheslab/eharve06/bdl-transfer-learning/experiments/retrained_CIFAR-10_torchvision' --lr_0=0.0001 --model_name='nonlearned_lr_0=0.0001_n=10_random_state=2001_weight_decay=0.01' --n=10 --prior_path='/cluster/tufts/hugheslab/eharve06/resnet50_torchvision' --prior_type='nonlearned' --random_state=2001 --wandb --wandb_project='retrained_CIFAR-10_torchvision' --weight_decay=0.01"
+# TODO: See notebooks for generating scripts.
 )
-
+    
 eval "${experiments[$SLURM_ARRAY_TASK_ID]}"
 
 conda deactivate
