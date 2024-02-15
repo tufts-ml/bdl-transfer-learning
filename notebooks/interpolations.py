631008d75b98bfade16c45d06b0561f12d9aa104
Ethan Harvey <Ethan.Harvey@tufts.edu>
2024-02-15 12:25:28 -0500

    Update interpolations.py

--- notebooks/interpolations.py
+++ notebooks/interpolations.py
@@ -59,7 +59,7 @@ def interpolate_checkpoints(first_checkpoint, second_checkpoint, n=41):
 
 if __name__=='__main__':
     # Learned
-    experiments_path = '/cluster/tufts/hugheslab/eharve06/bdl-transfer-learning/experiments/tuned_CIFAR-10'
+    experiments_path = ''
     lr_0s = np.logspace(-1, -4, num=4)
     ns = [1000]
     prior_scales = np.logspace(0, 4, num=5)
@@ -68,7 +68,7 @@ if __name__=='__main__':
     weight_decays = np.append(np.logspace(-2, -6, num=5), 0)
     learned_hyperparameters = get_best_hyperparameters(experiments_path, lr_0s, ns, prior_scales, prior_type, random_states, weight_decays)
     # Nonlearned
-    experiments_path = '/cluster/tufts/hugheslab/eharve06/bdl-transfer-learning/experiments/tuned_CIFAR-10'
+    experiments_path = ''
     lr_0s = np.logspace(-1, -4, num=4)
     ns = [1000]
     prior_scales = [None]
@@ -79,18 +79,18 @@ if __name__=='__main__':
     
     for (learned_index, learned_row), (nonlearned_index, nonlearned_row) in zip(learned_hyperparameters.iterrows(), nonlearned_hyperparameters.iterrows()):
         # Load learned checkpoint
-        experiments_path = '/cluster/tufts/hugheslab/eharve06/bdl-transfer-learning/experiments/retrained_CIFAR-10'
+        experiments_path = ''
         model_name = f'learned_lr_0={learned_row.lr_0}_n={learned_row.n}_prior_scale={learned_row.prior_scale}_random_state={learned_row.random_state}_weight_decay={learned_row.weight_decay}'
         learned_checkpoint = torch.load(f'{experiments_path}/{model_name}.pth', map_location=torch.device('cpu'))
         # Load nonlearned checkpoint
-        experiments_path = '/cluster/tufts/hugheslab/eharve06/bdl-transfer-learning/experiments/retrained_CIFAR-10'
+        experiments_path = ''
         model_name = f'nonlearned_lr_0={nonlearned_row.lr_0}_n={nonlearned_row.n}_random_state={nonlearned_row.random_state}_weight_decay={nonlearned_row.weight_decay}'
         nonlearned_checkpoint = torch.load(f'{experiments_path}/{model_name}.pth', map_location=torch.device('cpu'))
 
         interpolations = interpolate_checkpoints(nonlearned_checkpoint, learned_checkpoint)
         assert int(learned_row.random_state) == int(nonlearned_row.random_state), 'Expected random_state in each row to be the same'
         random_state = int(learned_row.random_state)
-        dataset_path = '/cluster/tufts/hugheslab/eharve06/CIFAR-10'
+        dataset_path = ''
         augmented_train_dataset, train_dataset, val_or_test_dataset = utils.get_cifar10_datasets(root=dataset_path, n=1000, tune=False, random_state=random_state)
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128)
         val_or_test_loader = torch.utils.data.DataLoader(val_or_test_dataset, batch_size=128)
@@ -119,7 +119,7 @@ if __name__=='__main__':
         torch.save(train_losses, './nonlearned_train_interpolation_random_state={}.pth'.format(random_state))
         torch.save(val_or_test_losses, './nonlearned_test_interpolation_random_state={}.pth'.format(random_state))
         
-        prior_path = '/cluster/tufts/hugheslab/eharve06/resnet50_ssl_prior'
+        prior_path = ''
         number_of_samples_prior = 5 # Default        
         loc = torch.load(f'{prior_path}/resnet50_ssl_prior_mean.pt')
         cov_factor = torch.load(f'{prior_path}/resnet50_ssl_prior_covmat.pt')
